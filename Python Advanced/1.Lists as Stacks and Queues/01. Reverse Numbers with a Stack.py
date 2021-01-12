9764daba52ee7673c2536e089731878662a4ad30
lazaroni <lazkolozanov@gmail.com>
2021-01-12 12:07:31 +0200

    Exercises

--- Python Advanced/1.Lists as Stacks and Queues/01. Reverse Numbers with a Stack.py
+++ Python Advanced/1.Lists as Stacks and Queues/01. Reverse Numbers with a Stack.py
@@ -1,26 +1,3 @@
-PUSH = '1'
-DELETE = '2'
-PRINT_MAX = '3'
-PRINT_MIN = '4'
+data = list(map(int, input().split()))
 
-queries = int(input())
-
-stack = []
-
-for _ in range(queries):
-    query = input().split()
-    if query[0] == PUSH:
-        stack.append(int(query[1]))
-    elif query[0] == DELETE:
-        stack.pop()
-    elif query[0] == PRINT_MAX:
-        print(max(stack))
-    elif query[0] == PRINT_MIN:
-        print(min(stack))
-
-rev_stack = []
-while stack:
-    rev_stack.append(stack.pop())
-
-print(', '.join(map(str, rev_stack)))
-
+[print(data.pop(), end=" ") for _ in range(len(data))]
