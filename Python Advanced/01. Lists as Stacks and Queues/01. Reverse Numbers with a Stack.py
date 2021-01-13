afc6f3d8c13ac43d769f26d1453234d6af42fae3
lazaroni <lazkolozanov@gmail.com>
2021-01-13 11:02:58 +0200

    Exercises

--- Python Advanced/01. Lists as Stacks and Queues/01. Reverse Numbers with a Stack.py
+++
@@ -1,3 +0,0 @@
-data = list(map(int, input().split()))
-
-[print(data.pop(), end=" ") for _ in range(len(data))]
--- Python Advanced/tests.py
+++
@@ -1,5 +0,0 @@
-from datetime import time, timedelta
-
-start = timedelta(hours=8, minutes=0, seconds=0)
-for i in range(100):
-    print(start + timedelta(seconds=i))
