cb54663077d14a6aa0980de6e3380549e53e9ea1
lazaroni <lazkolozanov@gmail.com>
2021-02-02 12:33:12 +0200

    Exercises

--- Python Advanced/Exam prep/Python Advanced Exam - 24 October 2020/03_list_pureness.py
+++ Python Advanced/Exam prep/Python Advanced Exam - 24 October 2020/03_list_pureness.py
@@ -1,9 +1,21 @@
-from collections import deque
-
-
-def best_list_pureness(nums):
+def best_list_pureness(nums, k):
     rotations = 0
     best_pureness = 0
+    best_pureness_rot = 0
 
-    for 
+    for _ in range(k + 1):
+        combination_pureness = 0
+
+        for i in range(len(nums)):
+            combination_pureness += nums[i] * i
+
+        if combination_pureness > best_pureness:
+            best_pureness = combination_pureness
+            best_pureness_rot = rotations
+
+        nums.insert(0, nums.pop())
+        rotations += 1
+
+    return f'Best pureness {best_pureness} after {best_pureness_rot} rotations'
+
 
