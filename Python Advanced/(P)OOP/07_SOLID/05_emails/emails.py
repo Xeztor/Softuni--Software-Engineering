c7993e2b459143e198c54f4f3d25282f430aa205
lazaroni <lazkolozanov@gmail.com>
2021-03-25 17:02:34 +0200

    Exercises

--- Python Advanced/(P)OOP/07_SOLID/05_emails/emails.py
+++ Python Advanced/(P)OOP/07_SOLID/05_emails/emails.py
@@ -1,4 +1,24 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
+
+
+class IContent(ABC):
+    pass
+
+
+class MyContent(IContent):
+    def __init__(self, content):
+        self.content = content
+
+    @property
+    def content(self):
+        return self.__content
+
+    @content.setter
+    def content(self, content):
+        self.__content = ''.join(['<myML>', content, '</myML>'])
+
+    def __repr__(self):
+        return self.content
 
 
 class IEmail(object):
@@ -12,16 +32,11 @@ class IEmail(object):
     def set_receiver(self, receiver):
         pass
 
-    @abstractmethod
-    def set_content(self, content):
-        pass
-
 
 class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
@@ -39,20 +54,16 @@ class Email(IEmail):
             self.__receiver = receiver
 
     def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+        self.__content = content
 
     def __repr__(self):
 
-        template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
+        template = f"Sender: {self.__sender}\nReceiver: {self.__receiver}\nContent:\n{self.__content}"
 
-        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content)
-
-
-email = Email('IM', 'MyML')
-email.set_sender('qmal')
-email.set_receiver('james')
-email.set_content('Hello, there!')
-print(email)
+        return template
+#
+# email = Email('IM', 'MyML')
+# email.set_sender('qmal')
+# email.set_receiver('james')
+# email.set_content('Hello, there!')
+# print(email)
