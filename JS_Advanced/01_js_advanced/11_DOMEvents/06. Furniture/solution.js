793fe1fc38883a4f7eec7eb163ecb6a5aed6b0e2
unknown <lazkolozanov@gmail.com>
2022-10-12 00:41:33 +0300

    DOM Exercises

--- JS_Advanced/01_js_advanced/11_DOMEvents/06. Furniture/solution.js
+++ JS_Advanced/01_js_advanced/11_DOMEvents/06. Furniture/solution.js
@@ -1,4 +1,101 @@
 function solve() {
+  // test()
 
-  //TODO...
+  document.getElementsByTagName('button')[0].addEventListener('click', addTableRows);
+
+  document.getElementsByTagName('button')[1].addEventListener('click', buySelectedFurniture)
+
+
+  function addTableRows() {
+    let parsedArr = JSON.parse(document.querySelector("#exercise textarea").value);
+
+    let tableBody = document.getElementsByTagName('tbody')[0];
+    for (let obj of parsedArr) {
+      let tableRow = createTableRow(obj);
+      tableBody.appendChild(tableRow);
+    };
+
+  };
+
+
+  function createTableRow(obj) {
+    let tableRow = document.createElement('tr');
+    let img = document.createElement('img');
+    img.setAttribute('src', obj['img']);
+
+    let imgTd = document.createElement('td');
+    imgTd.appendChild(img);
+
+    let nameTd = document.createElement('td');
+    nameTd.innerHTML = `<p>${obj['name']}</p>`;
+
+    let priceTd = document.createElement('td');
+    priceTd.innerHTML = `<p>${obj['price']}</p>`;
+
+    let decFactorTd = document.createElement('td');
+    decFactorTd.innerHTML = `<p>${obj['decFactor']}</p>`;
+
+
+    let checkboxInput = document.createElement('input');
+    checkboxInput.setAttribute('type', 'checkbox');
+    // checkboxInput.disabled = true;
+
+    let checkBoxTd = document.createElement('td');
+    checkBoxTd.appendChild(checkboxInput);
+
+    tableRow.appendChild(imgTd);
+    tableRow.appendChild(nameTd);
+    tableRow.appendChild(priceTd);
+    tableRow.appendChild(decFactorTd);
+    tableRow.appendChild(checkBoxTd);
+
+    return tableRow
+  };
+
+  function buySelectedFurniture() {
+    let selectedInputs = document.querySelectorAll('td input[type="checkbox"]:checked')
+    let totalPrice = 0;
+    let decFacSum = 0;
+    let furnitureName = [];
+
+    for (let selected of selectedInputs) {
+      let parentRowElement = selected.parentNode.parentNode;
+      let [_, name, price, decFac, __] = parentRowElement.children;
+      [name, price, decFac] = [name.textContent, Number(price.textContent), Number(decFac.textContent)];
+
+      totalPrice += price;
+      decFacSum += decFac;
+      furnitureName.push(name);
+    };
+
+    let outputArea = document.getElementsByTagName('textarea')[1];
+    outputArea.value += `Bought furniture: ${furnitureName.join(', ')}` + '\n'
+    outputArea.value += `Total price: ${totalPrice.toFixed(2)}` + '\n'
+    outputArea.value += `Average decoration factor: ${decFacSum / furnitureName.length}`
+
+  };
+
+
+  function test() {
+    // let testJSON = JSON.stringify([
+    //   {
+    //     "img": "https://www.ikea.com/PIAimages/0447583_PE597395_S5.JPG",
+    //     "name": "Sofa",
+    //     "price": "259",
+    //     "decFactor": "0.4"
+
+    //   },
+    //   {
+    //     "img": "https://www.stylespafurniture.com/wp-content/uploads/2020/03/Cove_3_Door_Wardrobe_1.jpg",
+    //     "name": "Wardrobe",
+    //     "price": "120",
+    //     "decFactor": "1.2"
+    //   }
+    // ]);
+    // let testJSON = JSON.stringify([{ "name": "Sofa", "img": "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg", "price": 150, "decFactor": 1.2 }]);
+    let testJSON = JSON.stringify([{ "name": "Tablet", "img": "https://s12emagst.akamaized.net/products/16498/16497603/images/res_aec28fc5950c2a40e001ff99795e576b_200x200_l6m7.jpg", "price": 2000, "decFactor": 5.2 }, { "name": "Vase", "img": "https://cdn.shoplightspeed.com/shops/606957/files/11398239/200x200x2/small-lady-vase.jpg", "price": 15, "decFactor": 10 }]);
+
+    document.querySelector('#exercise textarea').value = testJSON;
+
+  };
 }
--- JS_Advanced/01_js_advanced/11_DOMEvents/08. Sudomu/solution.js
+++ JS_Advanced/01_js_advanced/11_DOMEvents/08. Sudomu/solution.js
@@ -1,4 +1,57 @@
 function solve() {
+    document.getElementsByTagName('button')[0]
+        .addEventListener('click', checkBoard);
 
-    //TODO...
-}
+    document.getElementsByTagName('button')[1]
+        .addEventListener('click', clearBoard);
+
+    function checkBoard() {
+        let tableBody = document.getElementsByTagName('tbody')[0];
+
+        let numMatrix = Array.from(tableBody.rows).map(row => {
+            return Array.from(row.children).reduce((acc, tdEl) => {
+                return [...acc, tdEl.getElementsByTagName("input")[0].value];
+            }, []);
+        });
+
+        let rows = numMatrix;
+        let cols = numMatrix.map((_, colIndex) => numMatrix.map(row => row[colIndex]));
+
+        let allVectors = [...rows, ...cols];
+        let invalid = allVectors.filter(vector => new Set(vector).size < 3);
+
+        let checkP = document.querySelector('#check p');
+        if (invalid.length > 0) {
+            document.getElementsByTagName('table')[0].style.border = '2px solid red';
+            checkP.style.color = 'red';
+            checkP.textContent = "NOP! You are not done yet...";
+            return;
+        };
+
+        document.getElementsByTagName('table')[0].style.border = '2px solid green';
+        checkP.style.color = 'green';
+        checkP.textContent = "You solve it! Congratulations!";
+    };
+
+    function clearBoard() {
+        let tableBody = document.getElementsByTagName('tbody')[0];
+        let rows = tableBody.rows;
+
+        for (let row of rows) {
+            let cols = row.children;
+            for (let col of cols) {
+                inputs = col.getElementsByTagName('input');
+                for (input of inputs) {
+                    input.value = '';
+                }
+            };
+        };
+
+        document.getElementsByTagName('table')[0].style.border = '';
+
+        let checkP = document.querySelector('#check p');
+        checkP.style.color = '';
+        checkP.textContent = "";
+    };
+
+};
